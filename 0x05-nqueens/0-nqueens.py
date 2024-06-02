15dbd3747df9507785a9851b01880dda0a79fc24
khayo254 <khayo1998@gmail.om>
2024-06-02 19:30:56 +0300

    value of N

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""N queens solution finder module.
-"""
 import sys
 
 
@@ -28,7 +25,7 @@ def get_input():
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except Exception:
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
     if n < 4:
